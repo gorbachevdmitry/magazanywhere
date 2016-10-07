817bb0a5b1ec17895959077a40e343b6a7983eca
gorbachevdmitry <dmgorbacheww@gmail.com>
2016-10-07 09:46:01 +0300

    да

--- magazin/magazin/deploy_settings/__init__.py
+++ magazin/magazin/deploy_settings/__init__.py
@@ -8,13 +8,4 @@ ALLOWED_HOSTS = [
     '.pythonanywhere.com',
 ]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'shoppythonanywhere',
-        'USER': 'dmitrygorbachev',
-        'PASSWORD': '150881',
-        'HOST': '.pythonanywhere.com',
-        'PORT': '',
-    }
-}
+
