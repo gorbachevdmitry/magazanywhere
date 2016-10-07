feda7d609abb42988a2793947e2de0427b7baaea
gorbachevdmitry <dmgorbacheww@gmail.com>
2016-10-07 14:09:53 +0300

    change set

--- magazin/magazin/settings.py
+++ magazin/magazin/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'assets/media/')
 CART_SESSION_ID = 'cart'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
